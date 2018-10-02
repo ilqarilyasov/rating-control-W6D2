cd1d19ef90e9d4c1a824f7c09f2a89758f8c6b90
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-02 15:33:26 -0500

    setup() completed

--- Rating Control/View Controller/CustomControl.swift
+++ Rating Control/View Controller/CustomControl.swift
@@ -11,20 +11,44 @@ import UIKit
 class CustomControl: UIControl {
     
     // MARK: - Properties
-    
+
     var value: Int = 1 // For ratings
     private let componentDimension: CGFloat = 40.0
     private let componentCount: Int = 5
     private let componentActiveColor: UIColor = .black
     private let componentInactiveColor: UIColor = .gray
+    private var stars = [UILabel]() // Local UILabel array
     
     // MARK: Initializer
     
     required init?(coder aCoder: NSCoder) {
         super.init(coder: aCoder)
-        //setup()
+        setup()
     }
     
+    func setup() {
+        for i in 1...5 {
+            let aFrame = CGRect(x: 0, y: 0, width: componentDimension, height: componentDimension)
+            let star = UILabel(frame: aFrame)
+            star.tag = i
+            self.addSubview(star)
+            stars.append(star)
+        }
+        // TODO: - Make them layout in a row
+        for star in stars {
+            star.layoutMargins = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: (componentDimension + 16.0))
+            star.font = UIFont.boldSystemFont(ofSize: 32.0)
+            star.text = "✩"
+            star.textAlignment = .center
+            star.textColor = componentActiveColor
+        }
+    }
     
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
     
 }
