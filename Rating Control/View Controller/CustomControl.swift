1a33d5464ddb6c3b1047c99797d511c51e5b3ebe
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-02 15:02:02 -0500

    CustomControl private properties created. Required init completed

--- Rating Control/View Controller/CustomControl.swift
+++ Rating Control/View Controller/CustomControl.swift
@@ -9,5 +9,22 @@
 import UIKit
 
 class CustomControl: UIControl {
-    var value: Int = 1
+    
+    // MARK: - Properties
+    
+    var value: Int = 1 // For ratings
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount: Int = 5
+    private let componentActiveColor: UIColor = .black
+    private let componentInactiveColor: UIColor = .gray
+    
+    // MARK: Initializer
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        //setup()
+    }
+    
+    
+    
 }
